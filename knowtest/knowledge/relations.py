143f9980e2f31409369d1df9208c09767297143c
malusamayo <773386@gmail.com>
2023-02-08 22:46:23 -0500

    update prompts

--- knowtest/knowledge/relations.py
+++ knowtest/knowledge/relations.py
@@ -1,23 +1,35 @@
 
 class Relations(object):
-    RELATEDTO = 0
-    TYPESOF = 1
-    PARTOF = 2
-    HASA = 3
-    HASPROPERTY = 4
-    USEDFOR = 5
-    ATLOCATION = 6
-    CAUSES = 7
-    MOTIVATEDBY = 8
-    OBSTRUCTEDBY = 9
-    MANNEROF = 10
-    LOCATEDNEAR = 11
-    HASAGENT = 12
-    HASPATIENT = 13
-    ASPECTOF = 14
+    TYPESOF = 0
+    PARTOF = 1
+    HASPROPERTY = 2
+    USEDFOR = 3
+    ATLOCATION = 4
+    CAUSES = 5
+    MOTIVATEDBY = 6
+    OBSTRUCTEDBY = 7
+    MANNEROF = 8
+    LOCATEDNEAR = 9
+    HASAGENT = 10
+    HASPATIENT = 11
+    RELATEDTO = 12
 
     def __init__(self):
-        self.relations = ["RELATEDTO", "TYPESOF", "PARTOF", "HASA", "HASPROPERTY", "USEDFOR", "ATLOCATION", "CAUSES", "MOTIVATEDBY", "OBSTRUCTEDBY", "MANNEROF", "LOCATEDNEAR", "HASAGENT", "HASPATIENT", "ASPECTOF"]
+        self.relations = [
+            "TYPESOF", 
+            "PARTOF", 
+            "HASPROPERTY", 
+            "USEDFOR", 
+            "ATLOCATION", 
+            "CAUSES", 
+            "MOTIVATEDBY", 
+            "OBSTRUCTEDBY", 
+            "MANNEROF", 
+            "LOCATEDNEAR", 
+            "HASAGENT", 
+            "HASPATIENT", 
+            "RELATEDTO"
+            ]
         self.translate_dict = dict(zip(self.relations , range(len(self.relations))))
 
     def has_relation(self, relation):
@@ -32,31 +44,29 @@ PROMPT_TEMPLATES = {
     RELATIONS.TYPESOF: 
         ["List {N} types of {topic}."],
     RELATIONS.PARTOF:
-        ["List {N} components of {topic}.", "List {N} parts of {topic}."], 
-    RELATIONS.MOTIVATEDBY:
-        ["List {N} reasons behind {topic}.", "List {N} motivations behind {topic}."], 
-    RELATIONS.OBSTRUCTEDBY:
-        ["List {N} forces against {topic}."], 
-    RELATIONS.MANNEROF:
-        ["List {N} forms {topic} could take.", "List {N} ways of {topic}."], 
+        ["List {N} parts of {topic}.", "List {N} aspects of {topic}."], 
     RELATIONS.LOCATEDNEAR:
         ["List {N} things that often locates near {topic}."], 
-    RELATIONS.ASPECTOF:
-        ["List {N} aspects of {topic}."], 
-    RELATIONS.USEDFOR:
-        ["List {N} usages of {topic}."], 
     RELATIONS.ATLOCATION:
         ["List {N} locations {topic} could appear in."], 
+    RELATIONS.USEDFOR:
+        ["List {N} uses of {topic}.", "List {N} purposes of {topic}."], 
+    RELATIONS.HASPROPERTY:
+        ["List {N} descriptions of {topic}."], 
+    # actions/events
+    RELATIONS.MANNEROF:
+        ["List {N} ways to do {topic}."], 
     RELATIONS.HASAGENT:
         ["List {N} groups that perform {topic}."], 
     RELATIONS.HASPATIENT:
-        ["List {N} groups that are influenced by {topic}."], 
-    RELATIONS.HASA:
-        ["List {N} things that belong to {topic}."], 
-    RELATIONS.HASPROPERTY:
-        ["List {N} attributes of {topic}."], 
+        ["List {N} groups that are targeted by {topic}."], 
+    RELATIONS.MOTIVATEDBY:
+        ["List {N} reasons behind {topic}.", "List {N} motivations behind {topic}."], 
+    RELATIONS.OBSTRUCTEDBY:
+        ["List {N} actions against {topic}.", "List {N} groups against {topic}."], 
     RELATIONS.CAUSES:
         ["List {N} consequences of {topic}."], 
+    # the most generic relation
     RELATIONS.RELATEDTO:
         ["List {N} concepts related to {topic}."], 
 }
