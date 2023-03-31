7a1d84c9516f871b2391a5c06da6397e9dc91626
malusamayo <773386@gmail.com>
2023-03-31 15:32:09 -0400

    tree structure

--- Capability/src/Tree/state/reducer.js
+++ Capability/src/Tree/state/reducer.js
@@ -2,24 +2,58 @@ import _cloneDeep from "lodash.clonedeep";
 import { searchDFS, createFolder} from "../../utils";
 import { FOLDER } from "./constants";
 
+// function updateTree(children, action) {
+//   return children.map(child => {
+//     if (child.id === action.payload.id) {
+//       return {
+//         ...child,
+//         isHighlighted: action.payload.isHighlighted
+//       };
+//     } else {
+//       return {
+//         ...child,
+//         children: updateTree(child.children, action)
+//       };
+//     }
+//   });
+// }
+
 const reducer = (state, action) => {
   let newState = _cloneDeep(state);
   let node = null;
   let parent = null;
-  if (action.payload && action.payload.id) {
-    let foundNode = searchDFS({
-      data: newState,
-      cond: (item) => {
-        return item.id === action.payload.id;
-      },
-    });
-    node = foundNode.item;
-    parent = node.parentNode;
-  }
+
+  // if (action.payload && action.payload.id) {
+  //   let foundNode = searchDFS({
+  //     data: newState,
+  //     cond: (item) => {
+  //       return item.id === action.payload.id;
+  //     },
+  //   });
+  //   node = foundNode.item;
+  //   parent = node.parentNode;
+  // }
+
+  // console.log(node, parent)
 
   switch (action.type) {
     case "SET_DATA":
       return action.payload;
+
+    // case "SET_HIGHLIGHTED":
+    //   return state.map(root => {
+    //     if (root.id === action.payload.id) {
+    //       return {
+    //         ...root,
+    //         isHighlighted: action.payload.isHighlighted
+    //       };
+    //     } else {
+    //       return {
+    //         ...root,
+    //         children: updateTree(root.children, action)
+    //       };
+    //     }
+    //   });
 
     // [TODO] - reduce cost of updating folder
     case FOLDER.REFRESH:
